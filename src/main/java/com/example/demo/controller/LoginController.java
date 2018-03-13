d5b64cda79f4b674c792a892f91abab3f649096b
Mikko Maja <mikmaj88@gmail.com>
2018-03-13 17:13:50 +0200

    Visual updates

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -61,16 +61,9 @@ public class LoginController {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("userName", "Welcome to RPGCC, " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
         modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
         modelAndView.setViewName("admin/home");
         return modelAndView;
     }
-
-    /*@GetMapping("/admin/characters")
-    public ModelAndView characters() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("admin/characters");
-        return modelAndView;
-    }*/
 }
